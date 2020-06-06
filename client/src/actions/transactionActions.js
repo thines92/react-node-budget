a769cbb6453a5081ca214e5c3655e7de9f424ec0
Tyler <tylerhines1992@gmail.com>
2020-06-06 11:29:43 -0400

    rewrote action creators

--- client/src/actions/transactionActions.js
+++ client/src/actions/transactionActions.js
@@ -1,48 +1,72 @@
-import axios from 'axios'
+import transactions from '../apis/transactions'
 
 import { FETCH_TRANSACTIONS, ADD_TRANSACTION, EDIT_TRANSACTION, DELETE_TRANSACTION } from './types'
 
-export const fetchTransactions = () => ({
-    type: FETCH_TRANSACTIONS,
-    payload: axios.get('http://localhost:9000/api/transaction')
-})
+// export const fetchTransactions = () => ({
+//     type: FETCH_TRANSACTIONS,
+//     payload: axios.get('http://localhost:9000/api/transaction')
+// })
 
-export const addTransaction = (transaction) => {
-    console.log("addTransaction()", transaction)
-    return (dispatch) => {
-        console.log('dispatch', dispatch)
-        const response = dispatch({
-            type: ADD_TRANSACTION,
-            payload: axios.post('http://localhost:9000/api/transaction', transaction).then(response => {
-                console.log('response', response)
-            })
-        }).then((data) => {
-            console.log('dispatch', dispatch)
-            dispatch(fetchTransactions())
-        })
-    }
+export const fetchTransactions = () => async (dispatch) => {
+    const response = await transactions.get('/transaction');
+
+    dispatch({ type: FETCH_TRANSACTIONS, payload: response.data })
 }
 
-export const deleteTransaction = (transactionId) => {
-    return (dispatch) => {
-        const response = dispatch({
-            type: DELETE_TRANSACTION,
-            payload: axios.delete(`http://localhost:9000/api/transaction/${transactionId}`)
-        }).then(() => {
-            dispatch(fetchTransactions())
-        }) 
-    }
+// export const addTransaction = (transaction) => {
+//     console.log("addTransaction()", transaction)
+//     return (dispatch) => {
+//         console.log('dispatch', dispatch)
+//         const response = dispatch({
+//             type: ADD_TRANSACTION,
+//             payload: axios.post('http://localhost:9000/api/transaction', transaction).then(response => {
+//                 console.log('response', response)
+//             })
+//         }).then((data) => {
+//             console.log('dispatch', dispatch)
+//             dispatch(fetchTransactions())
+//         })
+//     }
+// }
+
+export const addTransaction = (transaction) => async (dispatch) => {
+    const response = await transactions.post('/transaction', transaction);
+
+    dispatch({ type: ADD_TRANSACTION, payload: response.data})
 }
 
-export const updateTransaction = (transaction) => {
-    return (dispatch) => {
-        const response = dispatch({
-            type: EDIT_TRANSACTION,
-            payload: axios.put(`http://localhost:9000/api/transaction/${transaction._id}`, transaction)
-        }).then(() => {
-            dispatch(fetchTransactions())
-        })
-    }
+// export const deleteTransaction = (transactionId) => {
+//     return (dispatch) => {
+//         const response = dispatch({
+//             type: DELETE_TRANSACTION,
+//             payload: axios.delete(`http://localhost:9000/api/transaction/${transactionId}`)
+//         }).then(() => {
+//             dispatch(fetchTransactions())
+//         }) 
+//     }
+// }
+
+export const deleteTransaction = (id) => async (dispatch) => {
+    await transactions.delete(`/transaction/${id}`, id);
+
+    dispatch({ type: DELETE_TRANSACTION, payload: id })
+}
+
+// export const updateTransaction = (transaction) => {
+//     return (dispatch) => {
+//         const response = dispatch({
+//             type: EDIT_TRANSACTION,
+//             payload: axios.put(`http://localhost:9000/api/transaction/${transaction._id}`, transaction)
+//         }).then(() => {
+//             dispatch(fetchTransactions())
+//         })
+//     }
+// }
+
+export const updateTransaction = (transaction) => async (dispatch) => {
+    const response = await transactions.put(`/transaction/${id}`, transaction);
+
+    dispatch({ type: EDIT_TRANSACTION, payload: response.data })
 }
 
 export const updateEditState = (transaction) => {
