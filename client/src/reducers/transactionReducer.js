5dc207afaad8e621eebd61e3c5e9d7688132a8ce
Tyler <tylerhines1992@gmail.com>
2020-07-07 18:53:48 -0400

    turned case statements into variables

--- client/src/reducers/transactionReducer.js
+++ client/src/reducers/transactionReducer.js
@@ -1,30 +1,29 @@
 import _ from 'lodash';
 
+import {
+	FETCH_TRANSACTION,
+	FETCH_TRANSACTIONS,
+	ADD_TRANSACTION,
+	EDIT_TRANSACTION,
+	DELETE_TRANSACTION,
+} from './types';
+
 export default (state = {}, action) => {
 	switch (action.type) {
-		case 'FETCH_TRANSACTIONS': {
+		case FETCH_TRANSACTIONS: {
 			return { ...state, ..._.mapKeys(action.payload, '_id') };
 		}
-		case 'FETCH_TRANSACTION': {
+		case FETCH_TRANSACTION: {
 			return { ...state, [action.payload._id]: action.payload };
 		}
-		case 'ADD_TRANSACTION': {
+		case ADD_TRANSACTION: {
 			return { ...state, [action.payload._id]: action.payload };
 		}
-		case 'EDIT_TRANSACTION': {
+		case EDIT_TRANSACTION: {
 			return { ...state, [action.payload._id]: action.payload };
 		}
-		case 'DELETE_TRANSACTION': {
+		case DELETE_TRANSACTION: {
 			return _.omit(state, action.payload);
-		}
-		case 'UPDATE_EDIT_STATE': {
-			return {
-				...state,
-				[action.payload._id]: {
-					...action.payload,
-					editting: !action.payload.editting,
-				},
-			};
 		}
 		default:
 			return state;
